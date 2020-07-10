f94b2077b6a51ca30f7e0a5daeb8dcf582c932b5
smnjsph13 <65280825+smnjsph13@users.noreply.github.com>
2020-07-11 04:21:20 +0800

    fixed routes for gh pages by adding Switch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import Home from "./contents/home";
 import About from "./contents/about";
 import Education from "./contents/education";
-import Skills from './contents/skills';
-import Contact from './contents/contact';
+import Skills from "./contents/skills";
+import Contact from "./contents/contact";
 import "./App.css";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/navbar";
 
 function App() {
@@ -13,25 +13,26 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/education">
-          <Education />
-        </Route>
-        <Route path="/skills">
-          <Skills />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-      </div>   
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/education">
+            <Education />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
-
 
 export default App;
