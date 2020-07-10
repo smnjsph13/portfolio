dc1161f1681b39492a01e5c2b07d0d367e2ee99b
smnjsph13 <simon.azada@yahoo.com>
2020-07-10 19:45:29 +0800

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import Education from "./contents/education";
 import Skills from './contents/skills';
 import Contact from './contents/contact';
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/education">
-          <Education />
-        </Route>
-        <Route path="/skills">
-          <Skills />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-      </div>
-    </Router>
+    <HashRouter basename='/'>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/education">
+            <Education />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </div>
+      </Router>
+    </HashRouter>
   );
 }
 export default App;
