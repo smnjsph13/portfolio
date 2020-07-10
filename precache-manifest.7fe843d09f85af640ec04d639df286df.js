26bc3604cbe701dbbc96acc45fbe9cee9db99eea
smnjsph13 <simon.azada@yahoo.com>
2020-07-10 19:18:05 +0800

    Updates

---
+++ precache-manifest.7fe843d09f85af640ec04d639df286df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e6da1db4c2f5d3b0b736da3a8bda98",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8ab54a11657987cb9c8c",
+    "url": "/portfolio/static/css/main.e18e2def.chunk.css"
+  },
+  {
+    "revision": "def586009dc00c7b03a6",
+    "url": "/portfolio/static/js/2.1d2bb9a0.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/portfolio/static/js/2.1d2bb9a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab54a11657987cb9c8c",
+    "url": "/portfolio/static/js/main.3b99cd89.chunk.js"
+  },
+  {
+    "revision": "6cce1d17df110e83465e",
+    "url": "/portfolio/static/js/runtime-main.f9efd693.js"
+  },
+  {
+    "revision": "bb4647f10bc0151fb200ad4d4a72ad6a",
+    "url": "/portfolio/static/media/profilepic.bb4647f1.png"
+  }
+]);
