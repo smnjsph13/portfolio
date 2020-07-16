056d19bb5ac33e9a698c205e71f273b95a461e7f
smnjsph13 <simon.azada@yahoo.com>
2020-07-16 12:45:47 +0800

    added skills logo remove words

--- src/contents/skills.jsx
+++ src/contents/skills.jsx
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import htmllogo from "../img/html.png";
+import csslogo from "../img/css.png";
+import jslogo from "../img/js.png";
+import reactlogo from "../img/react.jpg";
+import bslogo from "../img/bs.png";
+import ghlogo from "../img/gh.jpg";
+import herologo from "../img/hero.png";
 
 class Skills extends Component {
   constructor(props) {
@@ -9,14 +16,23 @@ class Skills extends Component {
   }
   render() {
     return (
-      <div className="condiv skills">
-        <h1 className="skills" >My Skills</h1>
-        <ul>
-          {this.state.myskills.map((value) => {
-            return <li>{value}</li>;
-          })}
-        </ul>
-      </div>
+      <React.Fragment>
+        <div className="condiv skills">
+          <h1 className="skills" >My Skills</h1>
+            {/* <ul>
+              {this.state.myskills.map((value) => {
+                return <li>{value}</li>;
+              })}
+            </ul> */}
+            <img className="skilllogo" src={htmllogo} alt="logo" />
+            <img className="skilllogo" src={csslogo} alt="logo" />
+            <img className="skilllogo" src={jslogo} alt="logo" />
+            <img className="skilllogo" src={reactlogo} alt="logo" />
+            <img className="skilllogo" src={bslogo} alt="logo" />
+            <img className="skilllogo" src={ghlogo} alt="logo" />
+            <img className="skilllogo" src={herologo} alt="logo" />
+        </div>
+      </React.Fragment>
     );
   }
 }
