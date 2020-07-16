6e88821553a9480f59887282ab435fe5e95c986b
smnjsph13 <simon.azada@yahoo.com>
2020-07-16 12:44:51 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8a5cc2618f0905cfb6ca057474896f04.js"
+  "/portfolio/precache-manifest.509e7aff13241a0269024b76264c37bf.js"
 );
 
 self.addEventListener('message', (event) => {
